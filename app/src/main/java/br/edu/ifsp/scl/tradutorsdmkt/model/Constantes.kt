53bf83f8dcb84857ee32bf41752c5602ea1e27f2
Leandro Perez <leandropremier@gmail.com>
2019-04-06 09:54:04 -0300

    Partial commit

---
+++ app/src/main/java/br/edu/ifsp/scl/tradutorsdmkt/model/Constantes.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.scl.tradutorsdmkt.model
+
+object Constantes {
+    // Constantes usadas para acesso ao WS de tradução
+    val URL_BASE = "https://od-api.oxforddictionaries.com/api/v1/"
+    val END_POINT = "entries"
+    val APP_ID_FIELD = "app_id"
+    val APP_ID_VALUE = "1b443991" // Preencher com seu app_id
+    val APP_KEY_FIELD = "app_key"
+    val APP_KEY_VALUE = "c82086f220bc8f8c840cf457197d690e" // Preeencher com seu app_key
+}
